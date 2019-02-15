644618ead9f1282f17e2607eeb007436f605069b
Dorofei <da.nikiforov@s-vfu.ru>
2019-02-15 15:10:08 +0900

    calc max

--- 1.py
+++ 1.py
@@ -1,18 +1,20 @@
 from dolfin import *
 import numpy as np
 
+h = 8
+tau = 1.0/128.0
 
-mesh = UnitSquareMesh(10,10)
+mesh = UnitSquareMesh(h,h)
 V = FunctionSpace(mesh,"CG",1)
 u = TrialFunction(V)
 v = TestFunction(V)
 
-tau = 0.01;
+
 k = Constant(1.0)
 f = Constant(1.0)
 T = 1.0
 
-a = 1/tau*u*v*dx + inner(k*grad(u),grad(v))*dx
+a = (1/tau)*u*v*dx + inner(k*grad(u),grad(v))*dx
 
 u0 = Function(V)
 f = Constant(1)
@@ -28,9 +30,15 @@ def Bound(x,on_boundary):
 
 DBC = DirichletBC(V,Constant(0),Bound)
 
-file = File("./result1/1.pvd")
+#file = File("1.pvd")
+ar_max = np.empty(128)
+i = 0
 for t in np.arange(0.0,T,tau):
 	solve(a == L, u, DBC)
 	assign(u0, u)
-	file << (u, t)
+	ar_max[i] = u0.vector().max()
+	i=i+1
+	#file << (u, t)
+
+print ar_max
 
